247c6d83c338c8a01379b747b377681e210851e1
Andrea <andrearewards@hotmail.com>
2023-04-20 17:35:08 -0400

    set up goals controller

---
+++ controllers/goals.js
@@ -0,0 +1,9 @@
+import { Goal } from "../models/goal.js"
+
+function newGoal(req,res){
+  console.log('its working')
+}
+
+export{
+  newGoal as new
+}
--- routes/goals.js
+++ routes/goals.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
+import * as goalsCtrl from '../controllers/goals.js'
 
 const router = Router()
 
-
+router.get('/new', goalsCtrl.new)
 
 export {
   router
