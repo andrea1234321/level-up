bc268eb00d98a768765d1cdc012263e560fc14e7
Andrea <andrearewards@hotmail.com>
2023-04-23 21:37:39 -0400

    implement ability to look at a friends goals page

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -5,9 +5,6 @@ function index(req, res) {
   .then(profiles=> {
     Profile.findById(req.user.profile)
     .then(userProfile=> {
-      // console.log(req.user.profile)
-      // console.log(req.user.profile._id)
-      // console.log(profiles[0]._id)
       res.render('profiles/index', {
         profiles: profiles,
         userProfile,
@@ -83,8 +80,54 @@ function acceptFriend(req,res){
   })
 }
 
+function declineFriend(req,res){
+  Profile.findById(req.params.profileId)
+  .then(friendProfile=> {
+    Profile.findById(req.user.profile._id)
+    .then(userProfile=> {
+      userProfile.incomingFriendRequests.remove(req.params.profileId)
+      userProfile.save()
+      friendProfile.outgoingFriendRequests.remove(req.user.profile._id)
+      friendProfile.save()
+      .then(()=> {
+        res.redirect('/profiles')
+      })
+      .catch(err=> {
+        console.log(err)
+        res.redirect('/')
+      })
+    })
+    .catch(err=> {
+      console.log(err)
+      res.redirect('/')
+    })
+  })
+  .catch(err=> {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function show(req,res){
+  Profile.findById(req.params.profileId)
+  .populate('goals')
+  .then(friendProfile=> {
+    console.log(friendProfile)
+    res.render('profiles/show', {
+      friendProfile: friendProfile,
+      title: friendProfile.name,
+    })
+  })
+  .catch(err=> {
+    console.log(err)
+    res.redirect('/profiles')
+  })
+}
+
 export {
   index,
   requestFriend,
-  acceptFriend
+  acceptFriend,
+  declineFriend,
+  show
 }
--- routes/profiles.js
+++ routes/profiles.js
@@ -6,8 +6,10 @@ import * as profilesCtrl from '../controllers/profiles.js'
 const router = Router()
 
 router.get('/', isLoggedIn, profilesCtrl.index)
+router.get('/:profileId/show', profilesCtrl.show)
 router.post('/:profileId/requestFriend', profilesCtrl.requestFriend)
 router.post('/:profileId/acceptFriend', profilesCtrl.acceptFriend)
+router.post('/:profileId/declineFriend', profilesCtrl.declineFriend)
 
 export {
   router
